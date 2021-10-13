d665452a355382ac7df99736269d9e1be4c29f87
cod-all <80483197+cod-all@users.noreply.github.com>
2021-10-13 17:04:12 -0500

    Update test_connection.py
    Added DB and Collection variables

--- scripts/test_connection.py
+++ scripts/test_connection.py
@@ -5,15 +5,17 @@ import pymongo
 USER = os.environ["DOCUMENTDB_USER"]
 PASSWORD = os.environ["DOCUMENTDB_PASSWORD"]
 HOST = os.environ["DOCUMENTDB_ENDPOINT"]
+DATABASE = "testDb"
+COLLECTION = "people"
 
 client = pymongo.MongoClient(
     f"mongodb://{USER}:{PASSWORD}@{HOST}:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred"
 )
 
-db = client.restaurants
+db = client.{DATABASE}
 
-results = db.restaurants.count()
+results = db.{COLLECTION}.count()
 
 print(
-    f"Connected successfully! There are {results} documents in your restaurants collection."
+    f"Connected successfully! There are {results} documents in your {COLLECTION} collection."
 )
